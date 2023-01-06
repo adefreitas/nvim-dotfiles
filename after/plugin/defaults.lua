29e4e8c685fe952f4db2520dc5648fb8d667fcfd
adefreitas <6485562+adefreitas@users.noreply.github.com>
2023-01-06 23:07:46 +0100

    fix: transparency for windows terminal, should go back to linux

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,3 +3,6 @@ vim.opt.relativenumber = true;
 vim.opt.background = "dark"
 vim.cmd("colorscheme oxocarbon")
 require("nvim-autopairs").setup()
+vim.cmd("highlight Normal guibg=NONE")
+vim.cmd("highlight NonText guibg=NONE")
+vim.cmd("highlight SignColumn guibg=NONE")
